bdc14ef1ed39c71878ba5e0e6024058f7192cb1d
sidharth1017 <sidharthv605@gmail.com>
2024-02-11 20:15:06 +0530

    fixed api

--- client/src/Services/Api.js
+++ client/src/Services/Api.js
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:5500";
 
 export const getReports = async (category, subcategory, year, selectedAuthors) => {
   try {
-    const response = await axios.get(`${API_URL}/api/get-all-report?`, {
+    const response = await axios.get(`/api/get-all-report?`, {
       params: { category, subcategory, year, selectedAuthors },
     });
         return response.data;
